36bb122dbc6fa0f7478cb5a9e921022899354747
Jiahao Chen <1732+jiahao@users.noreply.github.com>
2018-08-29 20:20:49 -0400

    Add some simple tests
    Signed-off-by: Jiahao Chen <1732+jiahao@users.noreply.github.com>

---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+using ArbRadixFloatingPoint
+using Test
+
+let x = 2im, z = convert(ArbRadixFloat{x,2}, x)
+    @test z.exponent == 1
+    @test z.significand == [1, 0]
+    @test float(z) == x
+end
+
+let x = 2im, z = convert(ArbRadixFloat{-0.5im,2}, x)
+    @test z.exponent == 0
+    @test z.significand == [0, 1]
+    @test float(z) == x
+end
+
